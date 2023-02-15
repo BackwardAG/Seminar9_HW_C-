69bb4c41e5b58a36cd75af2d5f74087ac6ac304b
Armen G <ar.gukasov@gmail.com>
2023-02-15 17:22:08 +0300

    Two homework tasks added

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// // Задача 66: Задайте значения M и N. Напишите программу,
+// // которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите числа M и N для определения границ промежутка: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int SumNumbers(int M, int N)
+{   
+    if (M == N) 
+    {
+        return M;
+    }
+    if (M < N)
+    {
+        return N + SumNumbers(M, N - 1);
+    } 
+    else
+    {
+        return N + SumNumbers(M, N + 1);
+    }
+}
+
+System.Console.WriteLine(SumNumbers(M,N));
